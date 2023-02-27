93d75850d39768c217cb47afe7102fa80943b64a
blaufish <blaufish@users.noreply.github.com>
2023-02-27 22:00:45 +0100

    Install script for hugo v0.110.0 + LoveIt Theme

--- install.sh
+++ install.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+
 # 1. hugo new site www-hugo
 # 2. cd www-hugo
-# 3. git clone https://github.com/dillonzq/LoveIt.git themes/LoveIt
+
+HUGO_VERSION=0.110.0
+HUGO_FILE=$PWD/.deb/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+HUGO_URL=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+
+download_hugo() {
+	mkdir -p .deb
+	if [[ -f $HUGO_FILE ]]
+	then
+		return
+	fi
+	wget --directory-prefix .deb $HUGO_URL
+
+}
+set -x
+
+hugo version
+if [[ "$?" != "0" ]]
+then
+	download_hugo
+	sudo apt install $HUGO_FILE
+fi
+
+if [[ ! -d themes/LoveIt ]]
+then
+	git clone https://github.com/dillonzq/LoveIt.git themes/LoveIt
+fi
