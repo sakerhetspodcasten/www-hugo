00aa44c5e77abe91314a80400212d3fa9831a931
blaufish <blaufish@users.noreply.github.com>
2023-03-06 20:10:29 +0100

    Compare hash to hugo_0.110.0_checksums.txt
    Checksums published at:
    
    https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_checksums.txt

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 HUGO_VERSION=0.110.0
 HUGO_FILE=$PWD/.deb/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
 HUGO_URL=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+HUGO_SHA256=969eeabb570e9d674d374de37a9d4c8799edf91a9a4dd9115dd714d559b5ad46
 
 download_hugo() {
 	mkdir -p .deb
@@ -16,13 +17,19 @@ download_hugo() {
 	wget --directory-prefix .deb $HUGO_URL
 
 }
+
 set -x
 
 hugo version
 if [[ "$?" != "0" ]]
 then
 	download_hugo
-	sudo apt install $HUGO_FILE
+	if echo "$HUGO_SHA256  $HUGO_FILE" | shasum --algorithm 256 --check
+	then
+		sudo apt install $HUGO_FILE
+	else
+		exit 1
+	fi
 fi
 
 if [[ ! -d themes/LoveIt ]]
